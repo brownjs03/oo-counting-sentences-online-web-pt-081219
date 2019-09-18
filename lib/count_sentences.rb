b836a6f5d6e1628ee6b1244bf81d98da57fd77c3
Jeffrey Brown <brownjs03@gmail.com>
2019-09-18 02:58:04 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,22 @@ class String
   end
 
   def count_sentences
-
+    class Dog
+ 
+  attr_accessor :name, :owner
+ 
+  def initialize(name)
+    @name = name
+  end
+ 
+  def bark
+    "Woof!"
+  end
+ 
+  def get_adopted(owner_name)
+    self.owner = owner_name
+  end
+ 
+end
   end
 end
